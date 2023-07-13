0cf4754aaddee3df8868a95d80514eaf362df6dd
ywaquar <ywaquar@gmail.com>
2023-07-13 08:59:47 +0530

    updated files

--- setup.py
+++ setup.py
@@ -1,15 +1,13 @@
 from setuptools import setup, find_packages
-from typing import List
-
 #Declaring variables for setup function
 PROJECT_NAME='housing-predictor'
-VERSION="0.0.3"
+VERSION="0.0.4"
 AUTHOR='Waquar'
 DESCRIPTION="This is first Machine Learning End to End Project"
-# PACKAGES=["housing"]
-# REQUIREMENTS_FILE_NAME="requirements.txt"
 
-def get_requirements_list()->List[str]:
+REQUIREMENTS_FILE_NAME="requirements.txt"
+
+def get_requirements_list():
     """
     Description: This fynction is going to return list of requirement
     mentioned in requirements.txt file
@@ -20,8 +18,7 @@ def get_requirements_list()->List[str]:
 
     with open(REQUIREMENTS_FILE_NAME) as requirement_file:
         return requirement_file.readlines().remove("-e .")
-
-
+        
 setup(
 name =PROJECT_NAME,
 version=VERSION,
@@ -30,8 +27,3 @@ description=DESCRIPTION,
 packages=find_packages(),
 install_requires=get_requirements_list()
 )
-
-
-"""
-Pacakge is file na .py file inside folder is module
-"""
